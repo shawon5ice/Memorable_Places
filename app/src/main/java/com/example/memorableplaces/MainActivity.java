43d4201f4f88dd23093efea75a1ac96136ea0dea
robot <shawon5ice@gmail.com>
2020-12-24 13:33:52 +0600

    Done without saving for future.

--- app/src/main/java/com/example/memorableplaces/MainActivity.java
+++ app/src/main/java/com/example/memorableplaces/MainActivity.java
@@ -2,13 +2,39 @@ package com.example.memorableplaces;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    static ArrayAdapter arrayAdapter;
+    static ListView listView;
+    static ArrayList<String> places = new ArrayList<String>();
+    static ArrayList<LatLng> locations = new ArrayList<LatLng>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = findViewById(R.id.listView);
+        places.add("Add a new place...");
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,places);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                intent.putExtra("placeNumber",position);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/memorableplaces/MapsActivity.java
@@ -0,0 +1,147 @@
+package com.example.memorableplaces;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapLongClickListener {
+
+    LocationManager locationManager;
+    LocationListener locationListener;
+    private GoogleMap mMap;
+    LatLng latLng;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    public void centerOnMap(Location location,String title){
+        LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+        mMap.clear();
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address = "";
+        try {
+            List<Address> addressList = geocoder.getFromLocation(userLocation.latitude,userLocation.longitude,1);
+            if(addressList != null && addressList.size()>0){
+                if(addressList.get(0).getThoroughfare()!=null){
+                    if(addressList.get(0).getSubThoroughfare()!=null){
+                        address+= addressList.get(0).getSubThoroughfare();
+                    }
+                    address += addressList.get(0).getThoroughfare();
+                }
+            }
+            Log.i("addresslist", "centerOnMap: "+addressList.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Log.i("address", "centerOnMap: "+address);
+        mMap.addMarker(new MarkerOptions().position(userLocation).title(title));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation,12));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 50, locationListener);
+                    Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                    centerOnMap(lastKnownLocation,"Your location");
+                }
+            }
+        }
+    }
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setOnMapLongClickListener(MapsActivity.this);
+        Intent intent = getIntent();
+        if(intent.getIntExtra("placeNumber",0)==0){
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+            locationListener = new LocationListener() {
+                @Override
+                public void onLocationChanged(@NonNull Location location) {
+                    centerOnMap(location,"Your location");
+                }
+            };
+            if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,50,locationListener);
+                Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                centerOnMap(lastKnownLocation,"Your location");
+            }else {
+                ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},1);
+            }
+        }else{
+            Location place = new Location(LocationManager.GPS_PROVIDER);
+            place.setLatitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).latitude);
+            place.setLongitude(MainActivity.locations.get(intent.getIntExtra("placeNumber",0)).longitude);
+            centerOnMap(place,MainActivity.places.get(intent.getIntExtra("placeNumber",0)));
+        }
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        String address = "";
+
+        try {
+            List<Address> addressList = geocoder.getFromLocation(latLng.latitude,latLng.longitude,1);
+            if(addressList != null && addressList.size()>0){
+                if(addressList.get(0).getThoroughfare()!=null){
+                    if(addressList.get(0).getSubThoroughfare()!=null){
+                        address+= addressList.get(0).getSubThoroughfare();
+                    }
+                    address += addressList.get(0).getThoroughfare();
+                }
+            }
+            Log.i("addresslist", "onMapLongClick: "+addressList.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(address.equals("")){
+            SimpleDateFormat sdf = new SimpleDateFormat("HH:mm dd-MM-yyyy");
+            address += sdf.format(new Date());
+        }
+        Log.i("address", "onMapLongClick: "+address);
+        mMap.addMarker(new MarkerOptions().position(latLng).title(address));
+
+        MainActivity.places.add(address);
+        MainActivity.locations.add(latLng);
+        MainActivity.arrayAdapter.notifyDataSetChanged();
+    }
+}
